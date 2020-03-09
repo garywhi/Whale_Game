1833ef6fb403623dfd9b9a92f3ca44a6e5d2bece
garywhi <50818672+garywhi@users.noreply.github.com>
2020-03-08 23:33:17 -1000

    Created Prefabs
    Created Prefabs for pufferfish and bubble and made a random spawn script

---
+++ Whale Game/Assets/Scripts/Spawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] spawns;
+    public Transform spawnpoint;
+
+    private int rand;
+
+    public float startTimeBtwSpawns;
+    private float timeBtwSpawns;
+
+    private void Start()
+    {
+        if (timeBtwSpawns <= 0)
+        {
+            Instantiate(spawns[0], spawnpoint.transform.position, Quaternion.identity);
+        }
+    }
+}
